304976dc691df531a8d9c2e99869e025236740c9
Mauricegao <yunzhi@ualberta.ca>
2019-01-30 15:27:00 -0700

    build version

---
+++ build/precache-manifest.f7376fdf0020623084361944233f2b35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d1442791c5ad75db717",
+    "url": "/static/js/main.7d144279.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "fb257905a33435f5a2682c8124ccf8c7",
+    "url": "/index.html"
+  }
+];
